dec313fff86a10008c42b5d982f1a8771688b8d5
Murari-Sharma <murarishrama@outlook.com>
2025-03-29 19:52:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let imgBox=document.querySelector(".imgBox");
-let inputBox=document.querySelector(".inputBox");
-let btn=()=>{
-    
-imgBox.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputBox.value}`
-}
